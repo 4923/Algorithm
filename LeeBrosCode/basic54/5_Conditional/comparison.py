b3006a7611716e2c1edd356d2d385ea904bbd36c
4923 <202003774y@gmail.com>
2021-01-17 20:35:05 +0900

    Complete : basic54/conditional/comparison.py

---
+++ LeeBrosCode/basic54/5_Conditional/comparison.py
@@ -0,0 +1,41 @@
+import sys
+
+# input
+a, b = map(int, sys.stdin.readline().strip().split())
+
+# output
+# a >= b?
+if a >= b:
+    sys.stdout.write(str(1)+"\n")
+else:
+    sys.stdout.write(str(0)+"\n")
+
+# a > b?
+if a > b:
+    sys.stdout.write(str(1)+"\n")
+else:
+    sys.stdout.write(str(0)+"\n")
+
+# b >= a?
+if a <= b:
+    sys.stdout.write(str(1)+"\n")
+else:
+    sys.stdout.write(str(0)+"\n")
+
+# b > a?
+if a < b:
+    sys.stdout.write(str(1)+"\n")
+else:
+    sys.stdout.write(str(0)+"\n")
+
+# a == b?
+if a == b:
+    sys.stdout.write(str(1)+"\n")
+else:
+    sys.stdout.write(str(0)+"\n")
+
+# a != b?
+if a != b:
+    sys.stdout.write(str(1)+"\n")
+else:
+    sys.stdout.write(str(0)+"\n")
