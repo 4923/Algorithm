b22b17e727ddfdd73966cecad98b7d7a347d6909
4923 <202003774y@gmail.com>
2021-01-17 20:30:07 +0900

    Complete : basic54/operator/addition_average.py

---
+++ LeeBrosCode/basic54/4_operator/addition_average.py
@@ -0,0 +1,11 @@
+import sys
+
+# input
+a, b = map(int, sys.stdin.readline().strip().split())
+
+# output
+# addition
+sys.stdout.write(str(a+b) + " ")
+
+# average
+sys.stdout.write(f'{(a+b)/2:.1f}')
