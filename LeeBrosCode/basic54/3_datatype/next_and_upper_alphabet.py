0c10869986ab31fa77c8d04519404f756c60e122
4923 <202003774y@gmail.com>
2021-01-17 20:19:16 +0900

    Complete : basic54/datatype/squared_value.py

---
+++ LeeBrosCode/basic54/3_datatype/next_and_upper_alphabet.py
@@ -0,0 +1,13 @@
+import sys
+
+# input
+origin_alphabet = sys.stdin.readline().strip()
+
+# next alphabet
+next_alphabet = chr(ord(origin_alphabet)+1)
+
+# upper alphabet
+upper_alphabet = origin_alphabet.upper()
+
+# output
+sys.stdout.write(f'{next_alphabet} {upper_alphabet}')
