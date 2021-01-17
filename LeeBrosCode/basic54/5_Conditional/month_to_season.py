9348cb252a51386e0b10fc2c27b14f5eea518834
4923 <202003774y@gmail.com>
2021-01-17 20:41:11 +0900

    Complete : basic54/conditional/month_to_season.py

---
+++ LeeBrosCode/basic54/5_Conditional/month_to_season.py
@@ -0,0 +1,14 @@
+import sys
+
+# input (1<=m<=12)
+m = int(sys.stdin.readline().strip())
+
+# output
+if 3 <= m <= 5:
+    sys.stdout.write("봄")
+elif 6 <= m <= 8:
+    sys.stdout.write("여름")
+elif 9 <= m <=11
+    sys.stdout.write("가을")    
+elif m == 12 or 1 <= m <= 2:
+    sys.stdout.write("겨울")
