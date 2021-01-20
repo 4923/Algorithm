d9859e4135cd75c6606dbdb02a2c11dbc34c4d92
4923 <202003774y@gmail.com>
2021-01-20 22:42:37 +0900

    Complete : 격자 안에서 완전탐색 - 최고의33위치 겨울방학 코딩테스트캠프 2일차 Simulation
    1. 함수를 쓸 생각을 못했다. 함수를 안쓰면 for문이 4중이 되어버리는데 아무리 생각해도 그렇게 짜는건 미친짓같았음
    2. 그래서 for문을 최대한 안쓰려고 노력했는데 행과 열 각각의 시작지점과 끝지점을 따로 지정 할 수 있는 방법이 없었음
    3. 그러므로 gold 캐는 함수에서 이중for문으로 시작지점, 끝지점 만들고 main에서 같은 방식으로 행과 열 각각에 시작과 끝지점을 만들면 해결

--- LeeBrosCode/concepts/Simulation/bruteForce-best33.py
+++ LeeBrosCode/concepts/Simulation/bruteForce-best33.py
@@ -1,116 +1,50 @@
-'''
-최고의 33위치
-N * N 크기의 격자 정보가 주어집니다. 
-이때 해당 위치에 동전이 있다면 1, 없다면 0이 주어집니다. 
-N * N 격자를 벗어나지 않도록 3 * 3 크기의 격자를 적절하게 잘 잡아서 
-해당 범위 안에 들어있는 동전의 개수를 최대로 하는 프로그램을 작성해보세요.
-
-입력 형식
-첫 번째 줄에는 격자의 크기를 나타내는 N이 주어집니다.
-두 번째 줄부터는 N개의 줄에 걸쳐 격자에 대한 정보가 주어집니다. 
-각 줄에는 각각의 행에 대한 정보가 주어지며, 
-이 정보는 0또는 1로 이루어진 N개의 숫자로 나타내어지며 공백을 사이에 두고 주어집니다.
-
-3 ≤ N ≤ 20
-
-출력 형식
-N * N 격자를 벗어나지 않으면서, 3 * 3 크기 격자 내에 들어올 수 있는 최대 동전의 수를 출력해주세요.
-'''
-
 import sys
 
-# [INPUT]
-# line 1: N for N * N
-N = int(sys.stdin.readline().strip())
-# line 2 : info of N * N
-shape = []
-for _ in range(0, N):
-    shape.append([int(e) for e in sys.stdin.readline().strip().split()])
+# INPUT
+N = int(sys.stdin.readline())
 
-print(shape)
+grid = [
+    list(map(int, sys.stdin.readline().strip().split()))  # list로 따로 묶어줘야 이차원 배열이 된다.
+    for _ in range(N)  # 0, N으로 범위 지정하지 않아도 된다.
+]
 
-# # [SIMULATION]
-# # cnt : coin count
-# cnt = 0
-# cnt_li = []
-# for index, row in enumerate (shape):
-#     print(f'\t[CHECK] row : {row} / [index] : {index}')
-#     for index2, element in enumerate (row):
-#         print(f'\t[CHECK] elements of each row : {element} / [index2] : {index2}')
-#         if element == 1:
-#             cnt += 1
-#         if index2 + 2 > N:
-#             break
-
-#     if index + 2 >  N:
-#         break
-# print(f'var cnt : {cnt}')
-
-
-# cnt = 0  # temp summation
-# MAX = cnt  # result
-# 
-# for i in range(0, N):  # col
-#     for j in range(0, 3):  # row
-#         print(f'1. [Check] : i is {i}, j is {j} AND current grid is {shape[i][j]} which is shape{i}{j}')
-#         if j + i == N:
-#             print(f'\t1-1. [CNT check] : cnt is {cnt} and soon reset to 0')
-#             cnt = 0
-#             break
-#         elif shape [i][j] == 1:
-#             cnt += 1  # if grid is 1, plus 1 to variable cnt
-#             print(f'\t1-2. [CNT change] : plus 1 to cnt, which result in {cnt}')
-    
-#     if MAX < cnt:
-#         MAX = cnt  # change MAXimum
-#         print(f'2. [CHANGE MAX] MAX : {MAX}')
-#         cnt = 0  # reset variable cnt
-# 
-# print(f'\n[FINAL] MAX is {MAX}')
-
-
-# cnt = 0  # temp summation
-# MAX = cnt  # result
-# 
-# for k in range(0, N):  # whole square
-#     print(f'0 [Pre Check] : k is {k}, range of i')
-#     for i in range(k, N):  # col
-#         for j in range(3):  # row
-#             print(f'1. [Check] : i is {i}, j is {j} AND current grid is {shape[i][j]} which is shape{i}{j}')
-#             if j + i > N:
-#                 print(f'\t1-1. [CNT check] : cnt is {cnt} and soon reset to 0')
-#                 break
-#             elif shape [i][j] == 1:
-#                 cnt += 1  # if grid is 1, plus 1 to variable cnt
-#                 print(f'\t1-2. [CNT change] : plus 1 to cnt, which result in {cnt}')
-#     print("\n------------------------------\n")    
-
-#     if MAX < cnt:
-#         MAX = cnt  # change MAXimum
-#         print(f'2. [CHANGE MAX] MAX : {MAX}')
-#         cnt = 0  # reset variable cnt
-#     print("\n------------------------------\n")    
-
-# print(f'\n[FINAL] MAX is {MAX}')
-
-
-
-cnt = 0  # temp summation
-MAX = cnt  # result
-
-for k in range(0, N):
-    if k + 2 > N:
-        break
-
-    for i in range (k, k+2):  # col
-        for j in range (k, k+2):  # row
-            print(f'1. [Check] : i is {i}, j is {j} AND current grid is {shape[i][j]} which is shape{i}{j}')
-            if shape[i][j] == 1:
+# Count Gold
+def Count_Gold(start_row, start_col, end_row, end_col):
+    cnt = 0
+    for i in range(start_col, end_col):
+        for j in range(start_row, end_row):
+            # 1 0일때가 안나오는데? => main에서 row, col 각각 for문 돌려야 함. (for문 총 4개 사용)
+            # print(f'\t[Check] i is {i}, j is {j}\n\t\tand the grid coordinate is \tgrid[{i}][{j}]\t which is {grid[i][j]}')
+            if grid[i][j] == 1:
+                # i가 2일때 j가 1을 건너뛰는데...
                 cnt += 1
-                print(f'\t1-2. [CNT change] : plus 1 to cnt, which result in {cnt}')
-    
-    if MAX < cnt:
-        MAX = cnt
-        cnt = 0
+                # print(f'\t[Variable] cnt == [{cnt}] NOW')
+    return cnt
 
-print(f'\n[FINAL] MAX is {MAX}')
+# print(grid, Count_Gold(0,0,N,N))  # 3 == N, end_row & end_col == N
+
+# Main
+def main():
+    MAX = Count_Gold(0, 0, 3, 3)  # baseCase
+    # print(MAX)
+    # print("--------------------------MAIN START----------------------------")
+    for k in range(N):
+        # print(f'K is {k}')
+        for t in range(N):
+            start_row = k
+            start_col = t
+            end_row = k+3
+            end_col = t+3
+            if end_row > N or end_col > N:
+                # print(f'[!!ANSWER!!] {MAX}')
+                break # break 걸고 => for문 다 빠져나간 후 MAX 한번만 출력
+            elif MAX < Count_Gold(start_row, start_col, end_row, end_col):
+                # print("-------------------------------")
+                # print(f'[Gold Check] Max gold is {MAX} and init gold is {Count_Gold(start_row, start_col, end_row, end_col)}')
+                # print(f'{MAX} => {Count_Gold(start_row, start_col, end_row, end_col)}')
+                # print("-------------------------------")
+                MAX = Count_Gold(start_row, start_col, end_row, end_col)
+
+    print(MAX)  # OUTPUT
+if __name__ == '__main__':
+    main()
