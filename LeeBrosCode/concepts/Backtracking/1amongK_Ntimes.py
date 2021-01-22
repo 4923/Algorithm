64014a48a7c9a93b125d779432db5f69e34ea78b
4923 <202003774y@gmail.com>
2021-01-23 07:23:48 +0900

    Complete : Backtracking / K개 중 하나를 N번 선택하기 (simple) / K개 중에 1개를 N번 뽑기 FindPermutation(cnt)에서 Base Case가 필요하다고 생각했는데 딱히 필요 없었고  Recursion Case만 cnt 이용해서 잘 구현하면 됐음. selected_nums가 결과값인데 1~k까지 돌면서 각각의 수 i를 뽑았다고 가정하고, i를 뽑은 이후에 뽑을 수를 재귀로 구현. 그리고 마지막 k에 도달했을 때 append 했었던 배열의 가장 마지막 원소를 pop으로 제거한다.

--- LeeBrosCode/concepts/Backtracking/1amongK_Ntimes.py
+++ LeeBrosCode/concepts/Backtracking/1amongK_Ntimes.py
@@ -1,3 +1,5 @@
+# 1amongK_Ntimes.py
+
 # [Import Module]
 import sys
 
@@ -6,44 +8,32 @@ import sys
 # N: N번 반복 (1~8)
 k, n = map(int, sys.stdin.readline().strip().split())
 
+selected_nums = []
+
 # [FUNCTION]
 # FindPermutation(cnt) -> kCn : pick k among n (Recursion)
 # cnt: count
 def FindPermutation(cnt):
     
-    # Base Case
-    if n == 1:
-        return n
+    # n개 모두 뽑았을 경우
+    if cnt == n:
+        for selected_num in selected_nums:
+            print(selected_num, end=" ")
+        print()
+        return
     
-    com = []
     # Recursion Case
+    # 1~ K까지의 각 숫자가 뽑혔을 때의 경우
     # N만큼 for문이 중첩되어야 모든 경우의 수를 파악하는데 어떡하나?
     # 1~K 까지의 수 중 하나를 뽑는것을 FindPermutation(n)으로 삼고 다른 수를 뽑는걸 재귀로 구현
-    
 
-
-
-    
-    return
-    
-    
-
-# Print(): for문으로 모든 결과를 출력
-def Print():
-    print('[FUNCTION] print')
-
-    return 0
+    for i in range(1, k+1):
+        selected_nums.append(i)  # i가 뽑혔으니까
+        FindPermutation(cnt + 1)  # 다음 수를 뽑도록 loop
+        selected_nums.pop()  # 배열의 가장 마지막 원소 제거 (.append()했던거 .pop()으로 제거)
 
 def main():
-    cnt = 0
-    answer = []
-    FindPermutation(1)  # 1부터 K까지 1씩 증가
-    
-    for i in range(n):
-        for j in range(k):
-            answer.append(FindPermutation(j))
-    
-    return 0
+    FindPermutation(0)  # 1부터 K까지 1씩 증가
 
 if __name__ == '__main__':
     main()
