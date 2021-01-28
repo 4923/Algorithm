48bca48114ffc9d6ec52400f6c3ae98e7c8139f5
4923 <202003774y@gmail.com>
2021-01-28 23:36:28 +0900

    Complete (Googling): 2941 시도한 풀이방식이라 이해는 가는데 지금 코드는 왜 안되는지를 모르겠다

--- BOJ/7_string/9_2941_LJESNJAK.py
+++ BOJ/7_string/9_2941_LJESNJAK.py
@@ -42,55 +42,61 @@ string = sys.stdin.readline().strip()
 # string 내장함수를 쓰라는 말 같아서 find, replace, in 사용
 
 def isCroatia(string, croatia, count):
-    # num = 0  # loop check variable
-    bPoint = 0
-    while bPoint < len(croatia):
+    
+    # 01-28 구글링 후 풀이
+    for alpha_C in croatia:
+        string = string.replace(alpha_C, "@")
+    return len(string)
 
-        # [탐색] 크로아티아 알파벳
-        for alpha_C in croatia:
-            # num += 1  # loop check
-            # print(f'-------------------{num}-------------------')
-            # [크로아티아 알파벳의 위치 확인]
-            index = string.find(alpha_C)
-            # print(index)
-            # print(type(index))
-            # print()
+    # # num = 0  # loop check variable
+    # bPoint = 0
+    # while bPoint < len(croatia):
 
-            leng = len(alpha_C) - 1
+    #     # [탐색] 크로아티아 알파벳
+    #     for alpha_C in croatia:
+    #         # num += 1  # loop check
+    #         # print(f'-------------------{num}-------------------')
+    #         # [크로아티아 알파벳의 위치 확인]
+    #         index = string.find(alpha_C)
+    #         # print(index)
+    #         # print(type(index))
+    #         # print()
 
-            # print(f'alpha_C: {alpha_C}')
-            if string.find(alpha_C) != -1:
+    #         leng = len(alpha_C) - 1
 
-                # print(f'[ALPHA_C check] ::\
-                    # {string[string.find(alpha_C) : string.find(alpha_C)+leng+1]}\
-                    # \n{string[leng:]}')
-                # print()
+    #         # print(f'alpha_C: {alpha_C}')
+    #         if string.find(alpha_C) != -1:
 
-                # [결과값 계산]
-                count += 1
-                # 문자열 전체를 범위로 두고 replace 하면 한번에 하나씩 지워지는게 아니라 전부 지워지므로 인덱스로 접근
+    #             # print(f'[ALPHA_C check] ::\
+    #                 # {string[string.find(alpha_C) : string.find(alpha_C)+leng+1]}\
+    #                 # \n{string[leng:]}')
+    #             # print()
 
-                # **파이썬의 문자열은 수정이 불가능하다!**
-                # replace를 쓰려고 해도 string = string 형식을 만들어야하는데 그러느니 슬라이싱 하는게 나음
-                # string = string[index : index + alpha_len].replace(alpha_C, " ")  # (??)
-                string = string[:index] + " " + string[index + leng+1:]
-                # print(f'Changed string: {string}')
-                # print()
+    #             # [결과값 계산]
+    #             count += 1
+    #             # 문자열 전체를 범위로 두고 replace 하면 한번에 하나씩 지워지는게 아니라 전부 지워지므로 인덱스로 접근
 
-            # [중지조건] if bPoint >= len(croatia)
-            bPoint = 0
-            for alpha_C in croatia:
-                if string.find(alpha_C) == -1:
-                    bPoint += 1
-                    # print(f'\n\t\tbPoint : {bPoint}\n')
+    #             # **파이썬의 문자열은 수정이 불가능하다!**
+    #             # replace를 쓰려고 해도 string = string 형식을 만들어야하는데 그러느니 슬라이싱 하는게 나음
+    #             # string = string[index : index + alpha_len].replace(alpha_C, " ")  # (??)
+    #             string = string[:index] + " " + string[index + leng+1:]
+    #             # print(f'Changed string: {string}')
+    #             # print()
+
+    #         # [중지조건] if bPoint >= len(croatia)
+    #         bPoint = 0
+    #         for alpha_C in croatia:
+    #             if string.find(alpha_C) == -1:
+    #                 bPoint += 1
+    #                 # print(f'\n\t\tbPoint : {bPoint}\n')
                 
     
-    # [탐색] 일반 알파벳
-    string = string.replace(" ", "")
-    # print(f'Normal alphabets: {len(string)}')
-    count += len(string)
-    # print(f'Total count: {count}')
-    return count
+    # # [탐색] 일반 알파벳
+    # string = string.replace(" ", "")
+    # # print(f'Normal alphabets: {len(string)}')
+    # count += len(string)
+    # # print(f'Total count: {count}')
+    # return count
 
 def main():
     # variable
@@ -121,7 +127,9 @@ if __name__ == '__main__':
 # answer : 2
 # replace 성질 때문인듯: c=와 c=에서 loop가 각각 따로 지워져야하는데 한번에 지워져버림
 
-# 반례 확인 4 (임의)
-# input : 
-# output : 
-# answer : 
+
+'''
+2021-01-28
+replace 쓰는건 맞았는데 아예 안쓰는 문자로 치환하면 섞일일이 없으니 안전함
+엄청 쉽게 풀리네... 이전코드는 무슨... 채점 중간도 아니고 채점 시작하자마자 오답으로 걸렸는데 무슨차이인지 모르겠다
+'''
