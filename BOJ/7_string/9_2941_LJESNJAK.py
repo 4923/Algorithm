f987abe9a09a28fa62810cafb7bb15d60bab9d6a
4923 <202003774y@gmail.com>
2021-01-28 23:16:00 +0900

    Fix? : 문자열 슬라이싱 범위 set 이제 다시 nljj - lj == nj 문제가 발생함

--- BOJ/7_string/9_2941_LJESNJAK.py
+++ BOJ/7_string/9_2941_LJESNJAK.py
@@ -42,23 +42,47 @@ string = sys.stdin.readline().strip()
 # string 내장함수를 쓰라는 말 같아서 find, replace, in 사용
 
 def isCroatia(string, croatia, count):
+    num = 0
     bPoint = 0
     while bPoint < len(croatia):
 
         # [탐색] 크로아티아 알파벳
         for alpha_C in croatia:
-            print(f'alpha_C: {alpha_C}')
-            if string.find(alpha_C) != -1:
-                count += 1
-                string = string.replace(alpha_C, " ")
-                print(f'Changed string: {string}')
+            num += 1  # loop check
+            print(f'-------------------{num}-------------------')
+            # [크로아티아 알파벳의 위치 확인]
+            index = string.find(alpha_C)
+            print(index)
+            print(type(index))
+            print()
 
-        # [중지조건] if bPoint >= len(croatia)
-        bPoint = 0
-        for alpha_C in croatia:
-            if string.find(alpha_C) == -1:
-                bPoint += 1
-                # print(f'\n\t\tbPoint : {bPoint}\n')
+            leng = len(alpha_C) - 1
+
+            # print(f'alpha_C: {alpha_C}')
+            if string.find(alpha_C) != -1:
+
+                print(f'[ALPHA_C check] ::\
+                    {string[string.find(alpha_C) : string.find(alpha_C)+leng+1]}\
+                    \n{string[leng:]}')
+                print()
+
+                # [결과값 계산]
+                count += 1
+                # 문자열 전체를 범위로 두고 replace 하면 한번에 하나씩 지워지는게 아니라 전부 지워지므로 인덱스로 접근
+
+                # **파이썬의 문자열은 수정이 불가능하다!**
+                # replace를 쓰려고 해도 string = string 형식을 만들어야하는데 그러느니 슬라이싱 하는게 나음
+                # string = string[index : index + alpha_len].replace(alpha_C, " ")  # (??)
+                string = string[:index] + string[index + leng+1:]
+                print(f'Changed string: {string}')
+                print()
+
+            # [중지조건] if bPoint >= len(croatia)
+            bPoint = 0
+            for alpha_C in croatia:
+                if string.find(alpha_C) == -1:
+                    bPoint += 1
+                    # print(f'\n\t\tbPoint : {bPoint}\n')
                 
     
     # [탐색] 일반 알파벳
@@ -88,3 +112,8 @@ if __name__ == '__main__':
 # input : nljj
 # output : 2 -> which has to be 3
 # find와 replace로 문자열에서 크로아티아문자 lj를 제거하면 nj가 남는데 이걸 붙이면 또다른 크로아티아문자가 되므로 중간에 띄어쓰기를 해주고 다시 띄어쓰기를 제거하는 과정이 필요. (일반 알파벳 개수 셀 때)
+
+# 반례 확인 3 (예제 4)
+# input c=c=
+# output : 1 not 2 (O)
+# replace 성질 때문인듯: c=와 c=에서 loop가 각각 따로 지워져야하는데 한번에 지워져버림
