5192ad4471c7f9005e5a0a3275bdb5932cde0117
4923 <202003774y@gmail.com>
2021-02-01 22:20:49 +0900

    Complete : 배열놀이 list의 index는 0부터 시작하지만 입력받은 위치는 1부터 시작함: -1더해서 index out of range error 피할 것 list의 .index(target) 함수는 인덱스를 찾을 수 없을때 error를 반환함. 따라서 try-except 구문을 사용 할 것

---
+++ LeeBrosCode/basic54/8_1dimArray/arrays.py
@@ -0,0 +1,24 @@
+import sys
+
+n, q = map(int, sys.stdin.readline().strip().split())
+numbers = list(map(int, sys.stdin.readline().strip().split()))
+req = [
+    list(map(int, sys.stdin.readline().strip().split()))
+    for _ in range(q)
+]
+
+for i in range(q):
+
+    if req[i][0] == 1:
+        print(numbers[req[i][1]-1])
+    elif req[i][0] == 2:
+        try:
+            index = numbers.index(req[i][1])
+            print(index+1)
+        except:
+            print(0)
+    else:  # 3
+        for i in range(req[i][1]-1, req[i][2]):
+            print(numbers[i], end = " ")
+        print()
+    print('---------------------------------------')
