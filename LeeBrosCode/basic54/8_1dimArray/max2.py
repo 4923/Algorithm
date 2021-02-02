c4654adcedac656f36d185c82cc436e4216bf233
4923 <202003774y@gmail.com>
2021-02-02 10:00:18 +0900

    Complete : n개의 숫자 중 최대 2개

---
+++ LeeBrosCode/basic54/8_1dimArray/max2.py
@@ -0,0 +1,11 @@
+import sys
+
+# 입력
+n = int(sys.stdin.readline().strip())
+numbers = list(map(int, sys.stdin.readline().strip().split()))
+
+# 정렬
+numbers.sort(reverse = True)
+
+# 출력
+[print(number, end = " ") for number in numbers[0:2]]
