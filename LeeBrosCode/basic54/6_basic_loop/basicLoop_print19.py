a663abe7c811e61325e7943d92c19875bd4a0549
4923 <202003774y@gmail.com>
2021-02-02 11:36:55 +0900

    Complete : .

---
+++ LeeBrosCode/basic54/6_basic_loop/basicLoop_print19.py
@@ -0,0 +1,17 @@
+# 19단 출력
+
+# input : X
+
+# output
+for i in range(1, 20):
+    for j in range(1, 20, 2):
+        # / 왼쪽
+        if j % 2 != 0 and j < 19:
+            print(f'{i} * {j} = {i*j}',end = " ")
+        # / 오른쪽
+        if (j+1) % 2 == 0 and (j+1) <= 19:
+            print(f'/ {i} * {j+1} = {i*(j+1)}')
+        # 19를 곱할 때
+        if j == 19:
+            print(f'{i} * {j} = {i*j}')
+
