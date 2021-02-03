a4378b5f3663cf9776b2b4553a323248e211426d
4923 <202003774y@gmail.com>
2021-02-03 14:48:59 +0900

    Complete : 기초 54제 / 문자열 다루기 / 문자열 놀이 쓸 데 없는 변수를 너무 많이 사용한 거 같은데 좀 더 깔끔한 방법은 없을까? replace의 세번째 인자가 교체할 횟수를 지정하는 숫자라던데 (그래서 .count()로 사용된 문자 개수도 셌는데) 그냥 문자열 전체를 대상으로 문자를 바꾸는 거였음. 그리고 횟수 지정해도 왼쪽에서부터 교체해서 의미가 없다. 인덱스로 접근하는게 가장 빠른듯. 반대로 order 2의 경우는 문자를 전부 바꿔야해서... replace 안쓰면 for문을 돌려야 하는 것 같음. 이건 string으로 접근하는게 좋은 거 같고.

---
+++ LeeBrosCode/basic54/10_string/string.py
@@ -0,0 +1,43 @@
+# a와 b는 index
+# 1: a와 b를 교환
+# 2: a를 b로 변경
+
+import sys
+
+# input
+string, count =sys.stdin.readline().strip().split()
+count = int(count)
+question = [
+    sys.stdin.readline().strip().split()
+    for _ in range(count)
+]
+
+# solve
+
+for q in question:
+    
+    order = q[0]
+    
+    if order == '1':
+        index1 = int(q[1])-1
+        index2 = int(q[2])-1
+
+        # 특정 문자만 교환해야하므로 index로 접근
+        string = list(string)
+
+        temp = string[index1]
+        string[index1] = string[index2]
+        string[index2] = temp
+
+        string = ''.join(string)
+
+        # output
+        print(string)
+
+    else:  # order == 2
+        old = q[1]
+        new = q[2]
+
+        temp = string
+        string = temp.replace(old, new)
+        print(string)
