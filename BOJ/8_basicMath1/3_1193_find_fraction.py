6c22082a395c49eae6142da2e5498ccb0f87b651
4923 <202003774y@gmail.com>
2021-02-08 22:19:53 +0900

    Fail : 1193 예제는 맞는데 제출하니까 Recursion Error 발생

--- BOJ/8_basicMath1/3_1193_find_fraction.py
+++ BOJ/8_basicMath1/3_1193_find_fraction.py
@@ -20,10 +20,46 @@ X가 주어졌을 때, X번째 분수를 구하는 프로그램을 작성하시�
 출력
 첫째 줄에 분수를 출력한다.
 
-# 지그재그라는게 이해가 안가서 검색함 : https://nhs0912.tistory.com/56
+# 지그재그라는게 이해가 안가서 검색함 : https://nhs0912.tistory.com/56, https://jaimemin.tistory.com/1037
 '''
 
-# # 분자
-# numerator = 
-# # 분모
-# denominator = 
+# 문제가 도대체 무슨 의미인지 모르겠다
+# 순서: 1/1 1/2 2/1 3/1 2/2 1/3 1/4 2/3 /32 /41 5/1 4/2 3/3 2/4 1/5... ...
+
+import sys
+
+# 분자 + 분모: 2, 3, 4, 5... ...
+# 분자와 분모의 합계 찾기: (x번째 분수, 1)
+def num_denominator(x, sub):
+    if x < sub:
+        return sub+1
+    else:
+        return num_denominator(x-sub, sub+1)
+
+def fraction(x, denominator_numerator):
+
+    # 앞에서 이미 한 사이클 (ex 1/4 2/3 3/2 4/1) 돈 숫자는 무시함
+    for cnt in range(1, denominator_numerator):
+        x -= cnt
+
+        if x <= cnt:
+            fraction_num = x
+    
+            if (denominator_numerator-1) % 2 != 0:  # 홀수번째면 분자가 오름차순으로 증가
+                fraction = f'{denominator_numerator - fraction_num}/{fraction_num}'
+                print(fraction)
+            else:  # 짝수번째면 분모가 오름차순으로 증가 (홀수일때와 분자 분모 순서 뒤바뀜)
+                fraction = f'{fraction_num}/{denominator_numerator - fraction_num}'
+                print(fraction)
+            
+            break
+
+def main():
+    x = int(sys.stdin.readline().strip())
+    denominator_numerator = num_denominator(x, 1)
+    # print(denominator_numerator)
+    fraction(x, denominator_numerator)
+
+
+if __name__ == '__main__':
+    main()
