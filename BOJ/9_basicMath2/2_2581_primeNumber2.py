391fb7e7484c22776e3360e0cd7c3050075570de
4923 <202003774y@gmail.com>
2021-02-15 21:31:59 +0900

    Complete : 2581

--- BOJ/9_basicMath2/2_2581_primeNumber2.py
+++ BOJ/9_basicMath2/2_2581_primeNumber2.py
@@ -22,9 +22,13 @@ end = int(input())
 
 
 def isPrime(num):
+    if num == 1:
+        return False
+
     for i in range(2, num):
         if num % i == 0:
             return False
+            
     return True
 
 def main():
