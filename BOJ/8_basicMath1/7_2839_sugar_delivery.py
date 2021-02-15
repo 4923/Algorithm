640a38f1ef7e61db2d14d24df04f16a11a822888
4923 <202003774y@gmail.com>
2021-02-15 17:57:26 +0900

    Fail : 예제까지는 정답

--- BOJ/8_basicMath1/7_2839_sugar_delivery.py
+++ BOJ/8_basicMath1/7_2839_sugar_delivery.py
@@ -13,4 +13,31 @@
 
 # 중요한건 개수
 # 5kg 봉지로 채울 수 있을 만큼 채우고 나머지는 3kg로 해결
-# -1이 출력되는 조건: N이 5와 3의 공배수가 아닐 때
+# -1이 출력되는 조건: N이 5와 3으로 분할되지 않을 때 (공배수 문제 아님)
+
+# [입력]
+n = int(input())
+
+# [풀이]
+# 사용하는 봉지의 개수
+result = 0 
+while n > 0 :
+    # 3kg 봉지를 최소한으로 사용한다.
+    n -= 3
+    result += 1
+    # print(f'n: {n} result: {result}')
+
+    # [if] 남은 무게를 5kg봉지로 해결할 수 있는지 확인
+    if n % 5 == 0:
+        result += n//5
+        # print(f'[break] n == {n}, n//5 == {n//5}')
+        break
+
+    # [exception] 3과 5로 분할할 수 없을 때
+    elif n < 0 :
+        result = -1
+        # print(f'[exception] n == {n} result == {result}')
+        break
+
+# [출력]
+print(result)
