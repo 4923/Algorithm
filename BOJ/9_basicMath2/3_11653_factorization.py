930b4c81d39f8679737671c6a38ca3714f2a53fa
4923 <202003774y@gmail.com>
2021-02-15 21:41:22 +0900

    Fail : 예제는 맞았는데 제출시 Recursion Error 발생

---
+++ BOJ/9_basicMath2/3_11653_factorization.py
@@ -0,0 +1,46 @@
+'''
+https://www.acmicpc.net/problem/11653
+
+문제
+정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
+
+출력
+N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
+'''
+
+# [입력]
+num = int(input())
+
+# [소인수분해]
+def factorization(num, divisor):
+    # print(f'num: {num} divisor: {divisor}')
+
+    # 종료조건
+    if num == 1:
+        return
+
+    if num % divisor == 0:
+        divisors.append(divisor)
+        return factorization(num//divisor, divisor)
+    else:
+        return factorization(num, divisor+1)
+
+def main():
+    global divisors
+    divisors = []
+
+    factorization(num, 2)
+
+    # [출력]
+    # 예외
+    if num == 1:
+        pass
+    else:
+        # print(divisors)
+        [print(divisor) for divisor in divisors]
+
+if __name__ == '__main__':
+    main()
