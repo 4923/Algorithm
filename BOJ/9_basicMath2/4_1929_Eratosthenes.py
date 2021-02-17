bc780627162b46503640c1d184202c442b3abcef
4923 <202003774y@gmail.com>
2021-02-17 23:16:00 +0900

    Complete : (try 6) 함수 사용해서 정답 1929

--- BOJ/9_basicMath2/4_1929_Eratosthenes.py
+++ BOJ/9_basicMath2/4_1929_Eratosthenes.py
@@ -11,52 +11,40 @@ M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오
 한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
 '''
 
-# False의 활용
+# False의 활용, 함수의 활용
 
 # 1. 입력
 import sys
 start, end = map(int, sys.stdin.readline().strip().split())
 
-# 2. 해당 범위 안의 수를 미리 나열한 list 생성
-# 이 리스트에서 소수가 아닌 수 (n의 배수) 를 False로 변환한다.
-sieve = [i for i in range(start, end+1)]
+# 2. return으로 빠지는게 좋겠어서 함수로 이전의 두번째 for문 변경
+def isPrime(num):
+    if num == 1:
+        # print(f'\t[{num}] return False')
+        return False
+    
+    for check in range(2, int(num ** (1/2)) + 1):
+        if num % check == 0:
+            # print(f'\t[num: {num}] & [check: {check}] return False')
+            return False
+    
+    # print(f'\t[{num}] return True')
+    return True
 
-# 1은 무조건 소수가 아니므로 예외로 처리한다
-if 1 in sieve:
-    sieve[0] = False
+def main():
+    # 3. 체를 만들어서 False로 변환하고 False가 아닌 것만 출력하는것이 번거로워서 소수인것만 출력하도록 함
+    for num in range(start, end + 1):
+        if isPrime(num):
+            # 4. 결과 출력
+            print(num)
 
-# 3. 에라토스테네스의 체: 소수가 아닌 수 제거
-# 배수를 지울 때 곱하는 수와 곱해지는 수를 위한 for문을 따로 작성할 필요 없이 sieve의 원소에서 나누면 된다.
-# 참고한 질문: (https://www.acmicpc.net/board/view/54480)
 
-for index in range(len(sieve)):
+if __name__ == '__main__':
+    main()
 
-    # (변수/try 5) 인덱스로 접근하기 위해 변수 생성
-    isPrime = sieve[index]
 
-    # 앞에서 소거한 수인 경우 소수여부를 검사하지 않는다.
-    if isPrime == False:
-        print('[FALSE]')
-        continue
-
-    # (변수) 최대 약수는 sqrt(num) 이하이므로 따로 편의를 위해 변수 생성
-    sqrt_num = int(isPrime ** (1/2))
-    print(f' isPrime: {isPrime}, sqrt_num: {sqrt_num}')
-
-    # (범위) 2를 검사할 경우 범위가 2~2가 되므로 sqrt에 1이 아닌 2를 더한다.
-    for num in range(2,  sqrt_num + 2):  
-        print(f'\tnum : {num}')
-
-        if isPrime != num and isPrime % num == 0:
-            sieve[isPrime-start] = False
-            # print(f'\t\tsieve = {sieve}\n')
-            break
-        
-        # (elif) 2를 검사할 경우 sqrt_num은 1이 되므로 출력하지 않고 지나가게 된다. num == isPrime 조건을 추가한다.
-        elif num == isPrime or num == sqrt_num:
-            print(isPrime)
-            # (break) 위에서 범위를 sqrt_num + 2로 정했으므로 3의 경우 num이 3까지 도달하게 된다. 이 때 3이 두 번 출력되므로 break 조건을 건다.
-            break
-
+# ----------------------------------------------------------
 # try 3: 1 ~ 100 까지 소수 정상출력 확인
 # try 4: 반례 (1 101) 입력 시 101 출력되지 않음 => 시간초과
+# try 5 : for isPrime in sieve 에서 인덱스로 접근방식 바꿈
+# try 6 : 함수 사용
