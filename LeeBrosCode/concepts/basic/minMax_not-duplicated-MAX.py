4c6409196102629b460b32f9a7d58f88844575fd
4923 <202003774y@gmail.com>
2021-03-13 23:21:41 +0900

    Complete : 중복되지 않는 숫자 중 최대

---
+++ LeeBrosCode/concepts/basic/minMax_not-duplicated-MAX.py
@@ -0,0 +1,26 @@
+import sys
+
+def main():
+
+    # [입력] 원소의 수
+    N = int(sys.stdin.readline().strip())
+
+    numbers = list(map(int, sys.stdin.readline().strip().split()))
+    unique = []
+
+    # 중복 확인
+    for number in numbers:
+        if numbers.count(number) == 1:
+            unique.append(number)
+
+    # # [출력] 최대
+    if len(unique) != 0:
+        print(max(unique))
+    
+    # [출력] 최대값이 없을 때 (모두 중복)
+    else:
+        print(-1)
+
+
+if __name__ == '__main__':
+    main()
