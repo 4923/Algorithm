824aef331f02209cf21b2164e7aed1fdf32ca80e
4923 <202003774y@gmail.com>
2021-03-13 21:44:06 +0900

    Create : Basic / 최대 최소 / 중복되지 않는 숫자 중 최대

--- LeeBrosCode/concepts/basic/basicLoop_sequence.py
+++ LeeBrosCode/concepts/basic/basicLoop_sequence.py
@@ -11,30 +11,41 @@
 연속하는 수와 그 수의 개수를 다음 원소로 정한다
 '''
 
-N = int(input())
+N = int(input()) - 1
 
 sequences = [ '1' ]
 
 # 수열의 각 항은 연속되는 수(num)와 그 수의 개수(cnt)다.
 for ind in range(0, N):
+
+    print(f'\n==============\t{ind}\t==============\n')
     
     num = sequences[ind]  # str
     leng = len(num)  # int
     
-    cnt = 0  # int
+    new_cnt = 1  # int
+    new = ''  # str
 
     for l in range(leng):
-        new_num = num[l]  # str
-        print(f'new_num : {new_num}')
-        for c in range(l, leng):
-            if new_num == num[c]:
-                cnt += 1
-                print(f'\tcnt : {cnt}')
+        check_num = num[l]  # str
+        print(f'num : {num} check_num : {check_num}')
+        for c in range(l+1, leng):
+            if check_num == num[c]:
+                new_cnt += 1
+                print(f'\tcheck_num : {check_num} new_cnt : {new_cnt}')
             else:
-                cnt = 0
+                new += (check_num + str(new_cnt))
+                new_cnt = 1
+                print('\t\tCONTINUE')
                 continue
 
-            new = new_num + str(cnt)
-        sequences.append(new)
+            print('------------------------------')
+
+        new = check_num + str(new_cnt)
+        print(f'[new] = {new}')
+        new_cnt = 1
+    sequences.append(new)
+    print(f'[sequences] : {sequences}')
+    print('\n======================================\n')
 
 print(sequences)
