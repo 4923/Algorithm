620064c9a9d1fdad42243065e57be3dae15470ef
4923 <202003774y@gmail.com>
2021-03-24 23:44:57 +0900

    Fail : O(N) 시간 안에 동작하는 알고리즘 고민 (Approach 2) 범위 나눠서 min, max 사용하면 해결될 거 같았는데 비효율적이란것만 깨달음 좀 더 고민 할 것

--- LeeBrosCode/concepts/basic/minMax_Car-deal.py
+++ LeeBrosCode/concepts/basic/minMax_Car-deal.py
@@ -3,10 +3,15 @@ import sys
 n = int(sys.stdin.readline().strip()) 
 prices = list(map(int, sys.stdin.readline().strip().split()))  # n년간 자동차의 가격
 
-benefit = 0
-for buy in range(n):
-    for sell in range(buy, n):
-        if benefit < prices[sell] - prices[buy] :
-            benefit = prices[sell] - prices[buy]
+# Time Complextity : O(N^2)
+# benefit = 0
+# for buy in range(n):
+#     for sell in range(buy, n):
+#         if benefit < prices[sell] - prices[buy] :
+#             benefit = prices[sell] - prices[buy]
 
-print(benefit)
+# print(benefit)
+
+
+# Time Complexity : O(N)
+# 최소, 최대값이 언제 올 지 모르니 min, max를 이용할 수 없음.
