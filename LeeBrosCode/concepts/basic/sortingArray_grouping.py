5f2b2cb6bfa0ec77173fdbe895663a1ae9803b45
4923 <202003774y@gmail.com>
2021-04-10 23:28:05 +0900

    Feature : 입력

--- LeeBrosCode/concepts/basic/sortingArray_grouping.py
+++ LeeBrosCode/concepts/basic/sortingArray_grouping.py
@@ -4,3 +4,9 @@
 # 조건 2. 그룹의 개수 : N, 주어지는 수 : 2 * N개
 # 조건 3. 그룹의 원소의 합 중 최댓값이 최소?
 #   ㄴ 정렬해서 반으로 나뉘었을 때 큰 값 그룹의 최소값과 작은 값 그룹의 최댓값의 합?
+
+
+import sys
+
+N = int(sys.stdin.readline().strip())  # 만들어야 하는 그룹의 수
+numbers = list(map(int, sys.stdin.readline().strip().split()))  # 2*N의 숫자
